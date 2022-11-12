ac1e51f71cc885806aa5bc2587ff5b97e608a96d
Samuel <201501502@rajalakshmi.edu.in>
2022-11-12 16:39:29 +0530

    Creating modules based on conditions

---
+++ PCA/app.py
@@ -0,0 +1,4 @@
+import pca1
+print("hello")
+a = pca1.check()
+print(a)
---
+++ PCA/pca1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import plotly
+import plotly.express as px
+plotly.offline.init_notebook_mode (connected = True)
+import matplotlib.pyplot as plt
+import warnings
+
+def check():
+    return "hello working!"
